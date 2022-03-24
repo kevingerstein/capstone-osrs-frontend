aeacc47f244183ce48c857f440e9fa9d71208476
kevin <kg1941a@gmail.com>
2022-03-24 13:21:39 -0400

    takes out extraneous comments from itemlist

--- src/components/ItemList.vue
+++ src/components/ItemList.vue
@@ -9,9 +9,6 @@ export default {
     findItemsBySlot: function () {
       let items = [];
       if (this.itemSlot === "weapon") {
-        // return Items.filter((item) => {
-        //   return item.equipment && (item.equipment.slot == "weapon" || item.equipment.slot == "2h");
-        // });
         Items.filter((item) => {
           if (item.equipment && (item.equipment.slot == "weapon" || item.equipment.slot == "2h")) {
             items.push(item);
@@ -23,9 +20,6 @@ export default {
             items.push(item);
           }
         });
-        // return Items.filter((item) => {
-        //   return item.equipment && item.equipment.slot == this.itemSlot;
-        // });
       }
       let unique_items_map = [...new Map(items.map((item) => [item.name, item])).values()];
       return unique_items_map;
@@ -36,13 +30,6 @@ export default {
 
 <template>
   <datalist :id="`${itemSlot}`">
-    <!-- <option
-      v-for="item in findItemsBySlot()"
-      :key="item.id"
-      :data="item[0]"
-      :value="item[1].name"
-      :id="item[1].name"
-    ></option> -->
     <option
       v-for="item in findItemsBySlot()"
       :key="item.id"
